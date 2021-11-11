5618e36a503596ca73b868d5243845dfeb0bf214
Brad Preston <bap5393@gmail.com>
2021-11-11 08:04:33 -0500

    finished basic smart contracts section

--- deploy.js
+++ deploy.js
@@ -1,11 +1,24 @@
 // deploy code will go here
+require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { bytecode, interface } = require('./compile');
-require('dotenv').config();
 
 const provider = new HDWalletProvider(
   process.env.RINKEBY_MNEMONIC,
   process.env.NETWORK_URL
 );
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account: ' + accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to: ' + result.options.address);
+};
+deploy();
